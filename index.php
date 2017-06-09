81e7da8d7e803bf9ec22d6ca4cdb09ea014253c3
Mateusz Hajder <mdhm@wp.pl>
2017-06-09 19:49:13 +0200

    Dodanie plików Phurl Wersji 3.

--- index.php
+++ index.php
@@ -1,117 +1,37 @@
 <?php
+// (C) Phurl Project 2012
+
+// Define PHURL to allow includes
 define('PHURL', true);
+
+// No debugging here, oh dear!
 ini_set('display_errors', 0);
+
+//Ensure they've deleted the install directory.
 $prefix[0] = '';
 $filename = 'install';
 if (is_dir($filename)) {
-    die ("To get Phurl up and running, you first need to go through the <a href=\"install\">installation wizard</a> which will help you set up your new URL shortener in a matter of moments.<br/><br/>If you've already installed Phurl, then you MUST delete the install directory before it will function.");
+	die ("To get Phurl up and running, you first need to go through the <a href=\"install\">installation wizard</a> which will help you set up your new URL shortener in a matter of moments.<br/><br/>If you've already installed Phurl, then you MUST delete the install directory before it will function.");
 }
-?>
-<?php
-require_once("config.php");
-require_once("functions.php");
 
+// include the magic
+include "includes/config.php";
+include "includes/functions.php";
+
+// connect to the database
 db_connect();
 
-if (count($_GET) > 0) {
-    $url   = mysql_real_escape_string(trim($_GET['url']));
-    $alias = mysql_real_escape_string(trim($_GET['alias']));
-
-    if (!preg_match("/^(".URL_PROTOCOLS.")\:\/\//i", $url)) {
- 	$prefix = explode(":", $url);
- 	if ($prefix[0] == 'mailto') {
- 		$url = $url;
- 	} else {
-        $url = "http://".$url;
- 	}
-    }
-
-    $last = $url[strlen($url) - 1];
-
-    if ($last == "/") {
-        $url = substr($url, 0, -1);
-    }
-
-    $data = @parse_url($url);
-		if ($prefix[0] == 'mailto') {
-			$data['scheme'] = 'mailto';
-			$data['host'] = 'none';
-		}
-    if (strlen($url) == 0) {
-        $_ERROR[] = "Please enter a URL to shorten.";
-    }
-    else if (empty($data['scheme']) || empty($data['host'])) {
-        $_ERROR[] = "Please enter a valid URL to shorten.";
-    }
-    else {
-        $hostname = get_hostname();
-        $domain   = get_domain();
-
-        if (preg_match("/($hostname)/i", $data['host'])) {
-            $_ERROR[] = "The URL you have entered is not allowed.";
-        }
-    }
-
-    if (strlen($alias) > 0) {
-        if (!preg_match("/^[a-zA-Z0-9_-]+$/", $alias)) {
-            $_ERROR[] = "Custom aliases may only contain letters, numbers, underscores and dashes.";
-        }
-        else if (code_exists($alias) || alias_exists($alias)) {
-            $_ERROR[] = "The custom alias you entered already exists.";
-        }
-    }
-
-    if (count($_ERROR) == 0) {
-        $create = true;
-
-        if (($url_data = url_exists($url))) {
-            $create    = false;
-            $id        = $url_data[0];
-            $code      = $url_data[1];
-            $old_alias = $url_data[2];
-
-            if (strlen($alias) > 0) {
-                if ($old_alias != $alias) {
-                    $create = true;
-                }
-            }
-        }
-
-        if ($create) {
-            do {
-                $code = generate_code(get_last_number());
-
-                if (!increase_last_number()) {
-                    die("System error!");
-                }
-
-                if (code_exists($code) || alias_exists($code)) {
-                    continue;
-                }
-
-                break;
-            } while (1);
-
-            $id = insert_url($url, $code, $alias);
-        }
-
-        if (strlen($alias) > 0) {
-            $code = $alias;
-        }
-
-        $short_url = SITE_URL."/".$code;
-
-        $_GET['url']   = "";
-        $_GET['alias'] = "";
-
-        require_once("html/header.php");
-        require_once("html/index_done.php");
-        require_once("html/index_form.php");
-        require_once("html/footer.php");
-        exit();
-    }
+if (file_exists(get_phurl_option('theme_path') . "header.php")) {
+	include (get_phurl_option('theme_path') . "header.php");
+} else {
+	die ("<h2>Could not load theme</h2>");
 }
 
-require_once("html/header.php");
-require_once("html/index_form.php");
-require_once("html/footer.php");
+include (get_phurl_option('theme_path') . "forms/shorten.php");
+
+if (file_exists(get_phurl_option('theme_path') . "footer.php")) {
+	include (get_phurl_option('theme_path') . "footer.php");
+} else {
+	die ("<h2>Could not load theme</h2>");
+}
+?>
